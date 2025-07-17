87e8b5849c7e81688e325f73f4e5d204aa8cfd71
Tara-Ravanlou <99586540+Tara-Ravanlou@users.noreply.github.com>
2025-07-17 18:30:00 +0330

    Update speak_to_text.py
    deleting comments

--- speak_to_text.py
+++ speak_to_text.py
@@ -1,24 +1,6 @@
 import speech_recognition as sr
 import datetime
 
-
-#def record_and_transcribe_long():
-#    recognizer = sr.Recognizer()
-#
-#    with sr.Microphone() as source:
-#        recognizer.adjust_for_ambient_noise(source, duration=1)
-#        audio_data = recognizer.listen(source, timeout=None, phrase_time_limit=None)
-#
-#    try:
-#        full_text = recognizer.recognize_google(audio_data, language="en-US")
-#        return full_text
-#    except sr.UnknownValueError:
-#        return None
-#    except sr.RequestError:
-#        return None
-
-#کد جدید
-
 def speak_to_text(audio_file):
     recognizer = sr.Recognizer()
     with sr.AudioFile(audio_file) as source:
